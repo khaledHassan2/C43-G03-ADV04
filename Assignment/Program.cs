3ec12b283cae0c73de818f0c45f449db26346cc2
khaledHassan2 <khaledamalb22@gmail.com>
2025-01-24 13:15:19 +0200

    1- Considering the Code Below, Write Down the Body of all Listed Methods and Properties and Constructor:

---
+++ Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Considering the Code Below, Write Down the Body of all Listed Methods and Properties and Constructor:
+            //Book myBook = new Book("555", "Darwen", new string[] { "khaled", "Rana" }, new DateTime(2025, 3, 10), 55.5m);
+            //Console.WriteLine(myBook);
+            #endregion
+        }
+    }
+}

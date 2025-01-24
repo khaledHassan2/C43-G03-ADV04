df22827af50fad9d080f5aed00d334d8437a0d4c
khaledHassan2 <khaledamalb22@gmail.com>
2025-01-24 13:44:00 +0200

    Edit Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,8 +5,14 @@
         static void Main(string[] args)
         {
             #region 1- Considering the Code Below, Write Down the Body of all Listed Methods and Properties and Constructor:
-            //Book myBook = new Book("555", "Darwen", new string[] { "khaled", "Rana" }, new DateTime(2025, 3, 10), 55.5m);
-            //Console.WriteLine(myBook);
+            //Book MyBook = new Book("555", "Darwen", new string[] { "khaled", "Rana" }, new DateTime(2025, 3, 10), 55.5m);
+            //Console.WriteLine(MyBook);
+            //Console.WriteLine($"Title {BookFunction.GetTitle(MyBook)}");
+            //Console.WriteLine($"Authors ({ BookFunction.GetAuthors(MyBook)})");
+            //Console.WriteLine($"Price {BookFunction.GetPrice(MyBook)}");
+            #endregion
+            #region MyRegion
+
             #endregion
         }
     }
